0371b0006e216b9f8c85eda0997bb3fef9ba6aa8
Steve Smith <stevesmithsays@Steves-MacBook-Pro.local>
2018-06-21 16:01:54 -0500

    updated poject with photos

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import Mens from '../assets/images/mens.png';
+import Womens from '../assets/images/womens.png';
+import Kids from '../assets/images/kids.png';
+import Gear from '../assets/images/packsGear.png';
 
 const Shop = () => {
   return(
     <div id="shop-component">
-      <div id="shop-header">
-        <h1><span>Shop</span> New Products</h1>
-        <div className="header-line"></div>
+
+      <div className="shop-body">
+
+        <div id="shop-header">
+          <h1><span>Shop</span> New Products</h1>
+          <div className="header-line"></div>
+        </div>
+
+        <div className="photos-container">
+          <div className="mens-img"><img src={Mens}/></div>
+          <div className="wommens-img"><img src={Womens}/></div>
+          <div className="kids-img"><img src={Kids}/></div>
+          <div className="gear-img"><img src={Gear}/></div>
+        </div>
       </div>
     </div>
   )
